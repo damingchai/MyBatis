16851f593dfb11465f16f463ea249411b7a0b903
damingchai <36779207+damingchai@users.noreply.github.com>
2018-06-10 15:09:03 +0800

    Update Test.java

--- Demo/src/com/demo/test/Test.java
+++ Demo/src/com/demo/test/Test.java
@@ -3,7 +3,7 @@ package com.demo.test;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("我爱北京天安门,我不爱北京");
+		System.out.println("我爱北京天安门,wobuaibeijing");
 	}
 
 }
